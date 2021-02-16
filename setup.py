38c8e601305b6a73525fc42bd706d4de241a6c9c
Knut Andreas Meyer <knutam@gmail.com>
2021-02-16 13:38:58 +0100

    Modified setup.py to allow comments in requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 
+#  Generate requirement list from requirements.txt file
 with open('requirements.txt', 'r') as fid:
-    requirements = [line for line in fid]
+    req = [line for line in fid if not line.strip().startswith('#')]
 
+# Call setup command to make installable by pip install .
 setup(name='<package_name>',
       version='<major>.<minor>',
       description='',
@@ -10,5 +12,5 @@ setup(name='<package_name>',
       author_email='',
       url='',
       packages=['<package_name>'],
-      install_requires=requirements
+      install_requires=req
      )
