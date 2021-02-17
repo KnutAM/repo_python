f9f35125188bc1bbf4bd5cd803a2a9357b82de9b
Knut Andreas Meyer <knutam@gmail.com>
2021-02-17 17:12:48 +0100

    Updated installation procedure

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 
 #  Generate requirement list from requirements.txt file
+req = []
 with open('requirements.txt', 'r') as fid:
     req = [line for line in fid if not line.strip().startswith('#')]
 
 # Call setup command to make installable by pip install .
-setup(name='<package_name>',
-      version='<major>.<minor>',
+setup(name='package_name',
+      version='0.1',
       description='',
       author='',
       author_email='',
       url='',
-      packages=['<package_name>'],
+      packages=['package_name'],
       install_requires=req
      )
